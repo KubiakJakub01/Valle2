089cf2c53e5f59e6886740a84a9345e2745e5cbe
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-05-07 21:09:37 +0200

    Use positional encoding from pytorch repository

--- valle/models/modules.py
+++ valle/models/modules.py
@@ -2,57 +2,47 @@ import math
 
 import torch
 import torch.nn as nn
+from einops import rearrange
 
 
-class SinusodialEmbedding(nn.Module):
-    def __init__(self, d_model):
+class PositionalEncoding(nn.Module):
+    r"""Inject some information about the relative or \
+            absolute position of the tokens in the sequence.
+        The positional encodings have the same dimension as the embeddings, \
+            so that the two can be summed.
+        Here, we use sine and cosine functions of different frequencies.
+    .. math:
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=5000).
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
         super().__init__()
-        self.d_model = d_model
-        exponent = torch.arange(self.d_half, dtype=torch.float32)
-        exponent = exponent / self.d_half
-        omega = torch.exp(-math.log(1e4) * exponent)
-        self.omega: torch.Tensor
-        self.register_buffer('omega', omega, persistent=False)
+        self.dropout = nn.Dropout(p=dropout)
 
-    @property
-    def d_half(self):
-        assert self.d_model % 2 == 0, 'Only support even d_model.'
-        return self.d_model // 2
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
-        """
+        r"""Inputs of forward function
         Args:
-            x: (...)
-        Returns:
-            pe: (... d)
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [batch size, sequence length, embed dim]
+            output: [batch size, sequence length, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
         """
-        omega = self.omega
-
-        while omega.dim() <= x.dim():
-            omega = omega.unsqueeze(0)  # (... d)
-
-        x = x.unsqueeze(-1)  # (... 1)
-        x = omega * x
-        x = torch.cat([x.sin(), x.cos()], dim=-1)
-
-        return x
-
-    def get_pe(self, n: int):
-        """
-        Args:
-            n: int
-        Returns:
-            pe: (n d)
-        """
-        device = self.omega.device
-        return self.forward(torch.arange(n, device=device))
-
-    def add_pe(self, x):
-        """
-        Args:
-            x: (b t c)
-        """
-        e = self.get_pe(x.shape[1])  # t d
-        e = e[None]  # b t d
-        x = x + e
-        return x
+        x = rearrange(x, 'b t c -> t b c')
+        x = x + self.pe[: x.size(0), :]
+        return rearrange(self.dropout(x), 't b c -> b t c')
