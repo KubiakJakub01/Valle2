d88039578de2b3039285c207baa26197e40899a3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-05-19 23:23:56 +0200

    Add sr and polling factor to hparams

--- valle/hparams.py
+++ valle/hparams.py
@@ -9,6 +9,8 @@ class ValleHparams:
     num_quantizers: int = field(
         default=8, metadata={'help': 'Number of quantizers layers from the audio codec'}
     )
+    sampling_rate: int = field(default=16000, metadata={'help': 'Sampling rate'})
+    polling_factor: int = field(default=320, metadata={'help': 'Polling factor'})
 
     # Model
     d_model: int = field(default=256, metadata={'help': 'Model dimension'})
@@ -17,3 +19,7 @@ class ValleHparams:
     dropout: float = field(default=0.1, metadata={'help': 'Dropout rate'})
     activation: str = field(default='relu', metadata={'help': 'Activation function'})
     num_layers: int = field(default=6, metadata={'help': 'Number of layers'})
+
+    @property
+    def quantization_factor(self):
+        return self.sampling_rate // self.polling_factor
