2548b672b2965340df30b293956bffcd974a613a
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-05-24 22:37:24 +0200

    Init train_model script

--- valle/models/__init__.py
+++ valle/models/__init__.py
@@ -1,4 +1,14 @@
 from .valle_ar import ValleAR
 from .valle_nar import ValleNAR
 
+MODEL_DICT = {
+    'ValleAR': ValleAR,
+    'ValleNAR': ValleNAR,
+}
+
+
+def get_model_class(model_name: str):
+    return MODEL_DICT[model_name]
+
+
 __all__ = ['ValleAR', 'ValleNAR']
---
+++ valle/train_model.py
@@ -0,0 +1,25 @@
+import argparse
+from pathlib import Path
+from typing import get_args
+
+from .hparams import ValleHparams
+from .models import MODEL_DICT, get_model_class
+from .utils import log_info
+
+
+def train(hparams_fp: Path, model_name: str):
+    hparams = ValleHparams.from_json(hparams_fp)
+    model = get_model_class(model_name)(hparams)
+
+    # Train model
+    log_info(f'Training model {model_name} with hparams: {hparams}')
+    log_info(f'Model: {model}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hparams', type=Path, required=True)
+    parser.add_argument('--model', type=str, choices=get_args(MODEL_DICT), required=True)
+    args = parser.parse_args()
+
+    train(args.hparams, args.model)
