Fix masking in multi-head attn

valle/models/modules.py
@@ -162,7 +162,7 @@ class MultiHeadAttention(nn.Module):
         # apply attention mask
         if attn_mask is not None:
             merged_mask = self.merge_masks(batch_size, attn_mask, padding_mask)
-            attn = attn.masked_fill(merged_mask == 0, float('-inf'))
+            attn = attn.masked_fill_(merged_mask, float('-inf'))
 
         # softmax and weighted sum
         attn = torch.softmax(attn, dim=-1)
@@ -271,7 +271,11 @@ class EncoderLayer(nn.Module):
         """
         norm_opt = {} if self.hparams.norm == 'LayerNorm' else {'embedding': embedding}
         x_attn, attn_weights, kv_cache = self.self_attn(
-            self.norm1(x, **norm_opt), attn_mask, padding_mask, kv_cache, use_cache
+            self.norm1(x, **norm_opt),
+            attn_mask=attn_mask,
+            padding_mask=padding_mask,
+            kv_cache=kv_cache,
+            use_cache=use_cache,
         )
         x = x + self.dropout1(x_attn)
         x = x + self.dropout2(self.ffn(self.norm2(x, **norm_opt)))
@@ -293,7 +297,7 @@ class EncoderLayer(nn.Module):
         return activation_dict[self.hparams.activation]
 
 
-class Encoder(nn.Module):
+class Transformer(nn.Module):
     """Transformer Encoder"""
 
     def __init__(self, hparams: ValleHparams) -> None:

valle/models/valle_ar.py
@@ -5,7 +5,7 @@ from einops import rearrange
 from torch.nn.utils.rnn import pad_sequence
 
 from ..hparams import ValleHparams
-from .modules import PositionalEncoding, TokenEmbedding
+from .modules import PositionalEncoding, TokenEmbedding, Transformer
 
 
 class ValleAR(nn.Module):
@@ -13,31 +13,20 @@ class ValleAR(nn.Module):
         super().__init__()
         self.hparams = hparams
 
-        self.eos_token = hparams.num_audio_tokens
-        self.bos_token = hparams.num_audio_tokens + 1
+        self.eos_token = self.hparams.num_audio_tokens
+        self.bos_token = self.hparams.num_audio_tokens + 1
 
         # Embeddings
-        self.tokens_emb = TokenEmbedding(hparams.vocab_size, hparams.d_model)
-        self.audio_emb = TokenEmbedding(hparams.num_audio_tokens + 2, hparams.d_model)
-        self.tokens_position_emb = PositionalEncoding(hparams.d_model)
-        self.audio_position_emb = PositionalEncoding(hparams.d_model)
+        self.tokens_emb = TokenEmbedding(self.hparams.vocab_size, self.hparams.d_model)
+        self.audio_emb = TokenEmbedding(self.hparams.num_audio_tokens + 2, self.hparams.d_model)
+        self.tokens_position_emb = PositionalEncoding(self.hparams.d_model)
+        self.audio_position_emb = PositionalEncoding(self.hparams.d_model)
 
-        # Decoder
-        self.decoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=hparams.d_model,
-                nhead=hparams.n_heads,
-                dim_feedforward=hparams.dim_feedforward,
-                dropout=hparams.dropout,
-                activation=hparams.activation,
-                batch_first=True,
-            ),
-            num_layers=hparams.num_layers,
-            norm=nn.LayerNorm(hparams.d_model),
-        )
+        # Transformer
+        self.transformer = Transformer(self.hparams)
 
         # Project to output
-        self.proj = nn.Linear(hparams.d_model, hparams.num_audio_tokens + 1, bias=False)
+        self.proj = nn.Linear(self.hparams.d_model, self.hparams.num_audio_tokens + 1, bias=False)
 
     @property
     def device(self):
@@ -94,7 +83,7 @@ class ValleAR(nn.Module):
 
         # Decoder
         xy = torch.cat((x, y), dim=1)
-        z = self.decoder(xy, mask=mask)
+        z, *_ = self.transformer(xy, attn_mask=mask)
         z = z[:, x_len:]
 
         # Project to output

valle/models/valle_nar.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 
 from ..hparams import ValleHparams
-from .modules import Encoder, PositionalEncoding, TokenEmbedding
+from .modules import PositionalEncoding, TokenEmbedding, Transformer
+from .utils import create_pad_mask
 
 
 class ValleNAR(nn.Module):
@@ -35,7 +36,7 @@ class ValleNAR(nn.Module):
         )
 
         # Decoder
-        self.decoder = Encoder(hparams)
+        self.transformer = Transformer(hparams)
 
         # Project to output
         self.proj = nn.Linear(hparams.d_model, hparams.num_audio_tokens + 1, bias=False)
@@ -50,7 +51,7 @@ class ValleNAR(nn.Module):
         """Forward pass of the model.
 
         Args:
-            tokens_list: List of token sequences (token_len).
+            tokens_list: List of token sequences (tokens_len).
             codes_list: List of audio codes (codes_len, quantization_layers).
 
         Returns:
@@ -59,27 +60,37 @@ class ValleNAR(nn.Module):
         assert len(tokens_list) == len(codes_list), 'Batch size mismatch.'
 
         # Prepare tokens
-        x_lens = list(map(len, tokens_list))
-        x = pad_sequence(tokens_list, batch_first=True)
-        x = self.tokens_emb(x)  # (b t c)
-        x = self.tokens_position_emb(x)
+        tokens_lens = list(map(len, tokens_list))
+        tokens_len = max(tokens_lens)
+        tokens = pad_sequence(tokens_list, batch_first=True)
+        tokens = self.tokens_emb(tokens)  # (b t c)
+        tokens = self.tokens_position_emb(tokens)
 
         # Prepare prompt and target audio
-        # Draw a random quantization layer
+        codes_lens = list(map(len, codes_list))
         layer = random.randint(1, self.hparams.num_quantizers - 1)
         codes = pad_sequence(codes_list, batch_first=True)
-        y_emb, prefix_len = self._prepare_audio_codes(codes, layer)
-        y_emb = self.audio_position_emb(y_emb)
+        codes, prefix_len = self._prepare_audio_codes(codes, layer)
+        codes = self.audio_position_emb(codes)
 
         # Prepare target audio
         target = codes[:, prefix_len:, layer]
 
+        # Prepare mask
+        codes_pad_mask = F.pad(
+            create_pad_mask(codes_lens, self.device),
+            (tokens_len, 0),
+            value=False,
+        )  # [tokens_len, codes_len]
+
         # Concatenate tokens and codes
-        xy = torch.cat([x, y_emb], dim=1)
+        xy = torch.cat([tokens, codes], dim=1)
 
         # Forward pass
-        z = self.decoder(xy, embedding=self.stage_embs[layer - 1].weight)
-        z = z[:, max(x_lens) + prefix_len]
+        z = self.transformer(
+            xy, padding_mask=codes_pad_mask, embedding=self.stage_embs[layer - 1].weight
+        )
+        z = z[:, max(tokens_lens) + prefix_len]
 
         # Project to output
         logits = self.proj(z)