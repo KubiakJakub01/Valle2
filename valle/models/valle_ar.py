43bd0893f6236d45c2df3d5c0ed60f2db4077080
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-06-07 23:26:32 +0200

    Add generate fn for valle_ar

--- valle/models/valle_ar.py
+++ valle/models/valle_ar.py
@@ -93,3 +93,82 @@ class ValleAR(nn.Module):
         loss = F.cross_entropy(logits, target)
 
         return loss
+
+    @torch.inference_mode()
+    def generate(
+        self,
+        tokens_list: list[torch.Tensor],
+        max_len: int = 100,
+    ):
+        """Generate audio codes.
+
+        Args:
+            tokens_list: List of tokens tensor (tokens_len)
+            max_len: Maximum length of audio codes
+        """
+        # Prepare tokens
+        x_len = max(map(len, tokens_list))
+        x = pad_sequence(tokens_list, batch_first=True)
+        x = self.tokens_emb(x)  # (b t c)
+        x = self.tokens_position_emb(x)
+
+        # Prepare audio
+        y = torch.full((x.size(0), 1), self.bos_token, device=self.device, dtype=torch.long)
+        y = self.audio_emb(y)  # (b t c)
+        y = self.audio_position_emb(y)
+
+        # Prepare mask
+        x_mask = torch.cat(
+            (
+                torch.zeros((x_len, x_len), dtype=torch.bool, device=self.device),
+                torch.ones((x_len, 1), dtype=torch.bool, device=self.device),
+            ),
+            dim=1,
+        )
+        y_mask = torch.cat(
+            (
+                torch.zeros((1, x_len), dtype=torch.bool, device=self.device),
+                torch.triu(torch.ones((1, 1), dtype=torch.bool, device=self.device), diagonal=1),
+            ),
+            dim=1,
+        )
+        mask = torch.cat((x_mask, y_mask), dim=0)
+
+        # Decoder
+        xy = torch.cat((x, y), dim=1)
+        for _ in range(max_len):
+            z, *_ = self.transformer(xy, attn_mask=mask)
+            z = z[:, x_len:]
+
+            # Project to output
+            logits = rearrange(self.proj(z), 'b t c -> b c t')
+
+            # Sample next token
+            y_next = torch.argmax(logits[:, :, -1], dim=-1, keepdim=True)
+            y = torch.cat((y, self.audio_emb(y_next)), dim=1)
+
+            # Update mask
+            y_mask = torch.cat(
+                (
+                    torch.zeros((1, x_len), dtype=torch.bool, device=self.device),
+                    torch.triu(
+                        torch.ones((1, y.size(1)), dtype=torch.bool, device=self.device), diagonal=1
+                    ),
+                ),
+                dim=1,
+            )
+            mask = torch.cat(
+                (
+                    torch.cat(
+                        (x_mask, torch.zeros((x_len, 1), dtype=torch.bool, device=self.device)),
+                        dim=1,
+                    ),
+                    y_mask,
+                ),
+                dim=0,
+            )
+
+            # Update xy
+            xy = torch.cat((x, y), dim=1)
+
+        return y[:, 1:]
