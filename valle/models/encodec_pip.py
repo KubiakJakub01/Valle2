554f2b38cd922002f55c73f374c8f01f0ee54229
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-06-21 23:01:47 +0200

    Add docstrings to encode and decode

--- valle/models/encodec_pip.py
+++ valle/models/encodec_pip.py
@@ -9,13 +9,29 @@ class EncodecPip:
         self.model.set_target_bandwidth(6.0)
 
     @torch.inference_mode()
-    def encode(self, audio):
+    def encode(self, audio: torch.Tensor) -> torch.Tensor:
+        """Encode audio into codes.
+
+        Args:
+            audio: 1D audio tensor of shape [T]
+
+        Returns:
+            codes: Tensor of shape [B, n_q, T]
+        """
         assert audio.dim() == 1, f'Expected 1D audio tensor, got {audio.dim()}D'
         audio = rearrange(audio, 't -> 1 1 t')
         encoded_frames = self.model.encode(audio)
         codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1)  # [B, n_q, T]
         return codes
 
-    def decode(self, codes):
+    def decode(self, codes: torch.Tensor) -> torch.Tensor:
+        """Decode codes into audio.
+
+        Args:
+            codes: Tensor of shape [B, n_q, T]
+
+        Returns:
+            audio: 1D audio tensor of shape [T]
+        """
         codes = rearrange(self.model.decode([(codes, None)]), '1 1 t -> t')
         return codes
