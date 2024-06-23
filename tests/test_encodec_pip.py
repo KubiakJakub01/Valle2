0b85f284403f53aa074f1141deae93ed9bcd9e56
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-06-23 22:56:17 +0200

    Add test_batch_encode

--- tests/test_encodec_pip.py
+++ tests/test_encodec_pip.py
@@ -17,3 +17,18 @@ def test_encode(audio: torch.Tensor, expected_shapes: tuple[int, int]):
     codes = encodec.encode(audio)
     assert codes.dim() == 2
     assert codes.shape == expected_shapes
+
+
+@pytest.mark.parametrize(
+    'audios, expected_shapes',
+    [
+        (torch.randn(4, 16000), (4, 8, 50)),
+        (torch.randn(4, 32000), (4, 8, 100)),
+        (torch.randn(4, 48000), (4, 8, 150)),
+    ],
+)
+def test_batch_encode(audios: torch.Tensor, expected_shapes: tuple[int, int, int]):
+    encodec = EncodecPip()
+    codes = encodec.batch_encode(audios)
+    assert codes.dim() == 3
+    assert codes.shape == expected_shapes
