907ff3179200a29084670c5f87b75af9ebf73261
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-07-02 21:41:33 +0200

    Add tests for batch_get_embedding fn

--- tests/test_encodec_pip.py
+++ tests/test_encodec_pip.py
@@ -95,3 +95,18 @@ def test_get_embedding(audios: torch.Tensor, expected_shapes: tuple[int, int]):
     codes = encodec.get_embedding(audios)
     assert codes.dim() == 2
     assert codes.shape == expected_shapes
+
+
+@pytest.mark.parametrize(
+    'audio, expected_shapes',
+    [
+        (torch.randn(4, 16000), (4, 8, 50)),
+        (torch.randn(4, 32000), (4, 8, 100)),
+        (torch.randn(4, 48000), (4, 8, 150)),
+    ],
+)
+def test_batch_get_embedding(audios: torch.Tensor, expected_shapes: tuple[int, int, int]):
+    encodec = EncodecPip()
+    codes = encodec.batch_get_embedding(audios)
+    assert codes.dim() == 3
+    assert codes.shape == expected_shapes
