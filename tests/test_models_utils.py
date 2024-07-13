ae9bdae2b5ca0951f952ad53667e4ca5893e6194
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-07-13 22:50:52 +0200

    Add test for create_pad_mask function

--- tests/test_models_utils.py
+++ tests/test_models_utils.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from valle.models.utils import build_attn_mask
+from valle.models.utils import build_attn_mask, create_pad_mask
 
 
 @pytest.mark.parametrize(
@@ -30,5 +30,30 @@ from valle.models.utils import build_attn_mask
 )
 def test_build_attn_mask(x_len: int, y_len: int, expected_mask: torch.Tensor):
     mask = build_attn_mask(x_len, y_len, device='cpu')
+    assert mask.shape == expected_mask.shape
+    assert torch.equal(mask, expected_mask)
+
+
+@pytest.mark.parametrize(
+    'x_len_list, expected_mask',
+    [
+        (
+            [5, 5, 5, 5],
+            torch.tensor(
+                [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
+                dtype=torch.bool,
+            ),
+        ),
+        (
+            [5, 4, 3, 2],
+            torch.tensor(
+                [[0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [0, 0, 0, 1, 1], [0, 0, 1, 1, 1]],
+                dtype=torch.bool,
+            ),
+        ),
+    ],
+)
+def test_create_pad_mask(x_len_list, expected_mask):
+    mask = create_pad_mask(x_len_list, device='cpu')
     assert mask.shape == expected_mask.shape
     assert torch.equal(mask, expected_mask)
